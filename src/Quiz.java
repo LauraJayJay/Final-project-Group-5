6e9a6fecd3c9ee0051ec80004a486ba1c8146881
Vaiva Mineikė <vaiva.mineike@gmail.com>
2023-04-24 20:29:37 +0300

    Update

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,36 +1,17 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.Scanner;
 
 public class Quiz {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String dbURL = "jdbc:mysql://localhost:3306/javaquiz";
-        // !!!Attention. It's necessary to fill out next two fields with your data
+        String dbURL = "jdbc:mysql://localhost:3306/java35";
         String username = "root";
         String password = "rooter";
 
-        // Try to connect with DB
 
-        try (Connection conn = DriverManager.getConnection(dbURL,username,password)){
-            System.out.println("Connected to db");
-            String sql = "SELECT * FROM questions";
-            Statement statement = conn.createStatement();
-            ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()){
-                String output = "question : " + resultSet.getString("question") + "\ncorrect answer: " + resultSet.getString("correct_answer");
-                System.out.println(output);
-            }
-
-        } catch (Exception e){
-            System.out.println(e);
-        }
-
-
-        System.out.println("Welcome to the JAVA Quize Game! ");
+        System.out.println("Wellcome to the JAVA Quize Game! ");
         System.out.println("What is your name?");
         String userName = scanner.nextLine();
 
