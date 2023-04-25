28563d8e8f8709739a75a87e7eca743fe7c30cb2
e-kondra <e-kondra@mail.ru>
2023-04-25 09:06:14 +0300

    Method Quiz + menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,77 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
+
 public class Menu {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static void quiz(Connection conn, String userName) throws SQLException {
+        int number = 0;
+        int score = 0;
+
+        //instead of this should be method what make request in DB and return ResultSet with data
+        String sql = "SELECT * FROM questions";
+        Statement statement = conn.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+
+
+        while (resultSet.next()) {
+            number ++;
+            String output = number + " Question : " + resultSet.getString("question");
+            System.out.println(output);
+
+            //memorise correct answer
+            String correctAnswer = resultSet.getString("correct_answer");
+
+            //Use ArrayList because we can shuffle it
+            ArrayList<String> answers = new ArrayList<>();
+            answers.add(resultSet.getString("correct_answer"));
+            answers.add(resultSet.getString("answer2"));
+            answers.add(resultSet.getString("answer3"));
+            answers.add(resultSet.getString("answer4"));
+
+            Collections.shuffle(answers);
+
+            Map<String, String> mapAnswers = new HashMap<>();
+
+            //put letters and shuffled answers in Map
+            for(int i = 0; i<answers.size(); i++){
+                mapAnswers.put(String.valueOf((char) (i+65)) ,answers.get(i));
+            }
+            //print answers
+            for(Map.Entry<String, String> entry: mapAnswers.entrySet()){
+                System.out.println(entry.getKey() + ") " + entry.getValue());
+            };
+
+            //get answer from user
+            String usersAnswer = scanner.nextLine().toUpperCase();
+
+            //Compare users answer with correct answer
+            if (mapAnswers.containsKey(usersAnswer)) { //Check if Map contains the Key
+                if (mapAnswers.get(usersAnswer).equals(correctAnswer)) { //Got correct answer
+                    //System.out.println(mapAnswers.get(usersAnswer));
+                    score += resultSet.getInt("score");
+                    //System.out.println(score);
+                };
+            }else {
+                System.out.println("Incorrect value");
+            };
+
+        }
+        // method that record the result in DB in table quizes (userName, score)
+
+        //It's just for checking, should be method
+        System.out.println("Your full score " + score);
+    }
+
+    /*public static void showRaiting(Connection conn){
+        //to show data, that was got from dbInteraction
+    }*/
 }
--- src/Quiz.java
+++ src/Quiz.java
@@ -5,34 +5,33 @@ import java.util.Scanner;
 public class Quiz {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        String dbURL = "jdbc:mysql://localhost:3306/java35";
+        String dbURL = "jdbc:mysql://localhost:3306/javaquiz";
+        // !!!Attention. It's necessary to fill out next two fields with your data
         String username = "root";
         String password = "rooter";
+        Menu menu = new Menu();
 
-
-        System.out.println("Wellcome to the JAVA Quize Game! ");
+        System.out.println("Welcome to the JAVA Quize Game! ");
         System.out.println("What is your name?");
+
         String userName = scanner.nextLine();
+        System.out.println("Hello, " + userName + ", let's try to complete JavaQuiz!");
 
-        //try (Connection conn = DriverManager.getConnection(dbURL,username,password)) {
-            //connection to DB
-       // }
 
-        //ask name
-        //menu method:
-         /*     - quiz
-                - show your results
-                - show score of all
-                - try again (y/n)
-          */
+        // Try to connect with DB
+        try (Connection conn = DriverManager.getConnection(dbURL, username, password)) {
+            System.out.println("Connected to db");
 
-        //loop with questions and answers
-        //ask the answer
-        //writing the answer in db
-        //calc the full score of user
-        //at the end writing results in db
+            menu.quiz(conn, userName);
 
+            //Show users result of the quiz
+
+            //Would you like to look at the raiting?
+            //menu.showRaiting
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
 
