64bc9688a10a203d28b81a887d48d40c5bfb7412
Laura <juozaityte1990@gmail.com>
2023-04-27 14:55:06 +0300

    Insert score method and some little grammar changes

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,5 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Quiz {
@@ -8,15 +10,15 @@ public class Quiz {
         String dbURL = "jdbc:mysql://localhost:3306/javaquiz";
         // !!!Attention. It's necessary to fill out next two fields with your data
         String username = "root";
-        String password = "rooter";
+        String password = "root12345";
         Menu menu = new Menu();
 
-        System.out.println("Welcome to the JAVA Quize Game! ");
+        System.out.println("Welcome to the JAVA Quiz Game! ");
         System.out.println("What is your name?");
 
         String userName = scanner.nextLine();
         System.out.println("Hello, " + userName + ", let's try to complete JavaQuiz!");
-        System.out.println("Please choose a themes of a quiz: 1 - first part of a course, 2 - second part part of a course, 0 - all course");
+        System.out.println("Please choose a themes of a quiz: \n1 - first part of a course, \n2 - second part part of a course, \n0 - all course");
         int part = scanner.nextInt();
 
          DBInteraction dbInteraction = new DBInteraction();
@@ -39,6 +41,15 @@ public class Quiz {
 
 
     //DBClass -> methods getting and writing in DB
+    public static void insertScore (Connection conn, String username, int fullScore) throws SQLException {
+        String sql = "INSERT INTO quizes (username, fullScore) VALUES (?,?)";
 
+        PreparedStatement preparedStatement = conn.prepareStatement(sql);
+        preparedStatement.setString(1, username);
+        preparedStatement.setInt(2, fullScore);
+
+        preparedStatement.executeUpdate();
+
+    }
 
 }
