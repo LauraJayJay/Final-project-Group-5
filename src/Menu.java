0061c332df2043c4d3b79ad9fa075654eab499b5
Valeria Zalygina <lerazalygina@gmail.com>
2023-04-27 12:11:50 +0300

    showUserResult Method was added

--- src/Menu.java
+++ src/Menu.java
@@ -15,15 +15,18 @@ public class Menu {
     public static void quiz(Connection conn, String userName, DBInteraction dbInteraction, int whichPart) throws SQLException {
         int number = 0;
         int score = 0;
+        int numCorrectAnswers = 0;
+
+        ArrayList<String> wrongAnswers = new ArrayList<>();
         ResultSet resultSet;
         if (whichPart == 0) {
-            resultSet = dbInteraction.getQuestions(conn);}
-        else {
+            resultSet = dbInteraction.getQuestions(conn);
+        } else {
             resultSet = dbInteraction.getQuestions(conn, whichPart);
         }
 
         while (resultSet.next()) {
-            number ++;
+            number++;
             String output = number + " Question : " + resultSet.getString("question");
             System.out.println(output);
 
@@ -42,13 +45,14 @@ public class Menu {
             Map<String, String> mapAnswers = new HashMap<>();
 
             //put letters and shuffled answers in Map
-            for(int i = 0; i<answers.size(); i++){
-                mapAnswers.put(String.valueOf((char) (i+65)) ,answers.get(i));
+            for (int i = 0; i < answers.size(); i++) {
+                mapAnswers.put(String.valueOf((char) (i + 65)), answers.get(i));
             }
             //print answers
-            for(Map.Entry<String, String> entry: mapAnswers.entrySet()){
+            for (Map.Entry<String, String> entry : mapAnswers.entrySet()) {
                 System.out.println(entry.getKey() + ") " + entry.getValue());
-            };
+            }
+            ;
 
             //get answer from user
             String usersAnswer = scanner.nextLine().toUpperCase();
@@ -58,17 +62,35 @@ public class Menu {
                 if (mapAnswers.get(usersAnswer).equals(correctAnswer)) { //Got correct answer
                     //System.out.println(mapAnswers.get(usersAnswer));
                     score += resultSet.getInt("score");
+                    numCorrectAnswers++;
                     //System.out.println(score);
-                };
-            }else {
+                } else {
+                    wrongAnswers.add(resultSet.getString("question"));
+                }
+                ;
+            } else {
                 System.out.println("Incorrect value");
-            };
-
+            }
         }
-        // method that record the result in DB in table quizes (userName, score)
 
-        //It's just for checking, should be method
-        System.out.println("Your full score " + score);
+        showUserResult(number, numCorrectAnswers, score, wrongAnswers);
+
+    }
+
+    public static void showUserResult(int numQuestions, int numCorrectAnswers, int score, ArrayList<String> wrongAnswers) {
+        System.out.println("Quiz result:");
+        System.out.println("Total questions: " + numQuestions);
+        System.out.println("HEY, you have "  + numCorrectAnswers + " correct answers");
+        System.out.println("Your full score is: " + score);
+
+        if (!wrongAnswers.isEmpty()) {
+            System.out.println("Here you was wrong:");
+            for (int i = 0; i < wrongAnswers.size(); i++) {
+                System.out.println((i+1) + ") " + wrongAnswers.get(i));
+            }
+        } else {
+            System.out.println("Congratulations! You answered all questions correctly!");
+        }
     }
 
     /*public static void showRaiting(Connection conn){
